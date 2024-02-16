310ccef141c42c320a6f860a4d3f08a41df0a710
Yonas Negese M <135352527+yonasman@users.noreply.github.com>
2024-02-16 16:28:10 +0300

    first amazone-backend

---
+++ index.jsx
@@ -0,0 +1,41 @@
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+dotenv.config()
+const stripe = require("stripe")(process.env.STRIPE_KEY)
+
+// initialiazr express
+const app = express()
+app.use(cors({origin:true}))
+ app.use(express.json())
+
+app.get("/", (req,res) => {
+    res.status(200).json({
+        message:"success!"
+    })
+})
+
+app.post("/payment/create",async(req,res) => {
+    const total = parseInt(req.query.total);
+    if(total > 0) {
+       const paymentIntent  = await stripe.paymentIntents.create({
+        amount:total,
+        currency : "usd"
+       })
+    //    console.log(paymentIntent)
+       res.status(201).json({
+        clientSecret: paymentIntent.client_secret,
+       })
+    } else {
+        res.status(403).json({
+            message:"total must be greater than 0"
+        })
+    }
+})
+
+app.listen(5000,(err) => {
+    if (err) throw err;
+    console.log("listening to PORT 5000: http://localhost:5000")
+})
+
+
